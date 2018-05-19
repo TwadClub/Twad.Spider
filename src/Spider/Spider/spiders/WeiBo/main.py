4e0b51c2bef76a88309b0d9a97ca1fd16a6c0970
AlbertAY <ay_1013@outlook.com>
2018-05-19 21:59:14 +0800

    11111

--- src/Spider/Spider/spiders/WeiBo/main.py
+++ src/Spider/Spider/spiders/WeiBo/main.py
@@ -1,10 +1,11 @@
 import scrapy
 
-class main(scrapy.spiders.Spider):
+
+class Main(scrapy.spiders.Spider):
     name = "TwadSpider"  # 爬虫的名字，执行时使用
-    allowed_domains = ["weibo.com"]  # 允许爬取的域名，非此域名的网页不会爬取
+    allowed_domains = ["jd.com"]  # 允许爬取的域名，非此域名的网页不会爬取
     start_urls = [
-        "https://weibo.com/a/hot/realtime"  # 起始url，此例只爬这一个页面
+        "https://www.jd.com/"  # 起始url，此例只爬这一个页面
     ]
 
     def parse(self, response):  # 真正的爬虫方法
