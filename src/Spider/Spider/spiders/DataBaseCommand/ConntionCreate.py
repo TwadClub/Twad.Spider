7670f3f40bb118d43eb08661d8eff4c4da5349b7
AlbertAY <ay_1013@outlook.com>
2018-06-05 22:50:15 +0800

    爬虫数据插入数据库

--- src/Spider/Spider/spiders/DataBaseCommand/ConntionCreate.py
+++ src/Spider/Spider/spiders/DataBaseCommand/ConntionCreate.py
@@ -1,17 +1,21 @@
 import pymysql
 
-    class ConnectionCreate:
-        def MysqlCreate(connStr):
-            # 打开数据库连接（ip/数据库用户名/登录密码/数据库名）
-            db = pymysql.connect("localhost", "root", "123456789", "twadspiderdata")
-            # 使用 cursor() 方法创建一个游标对象 cursor
-            cursor = db.cursor()
+class ConnectionCreate:
+    netAddress = ""
 
-            # 使用 execute()  方法执行 SQL 查询
-            cursor.execute("SELECT VERSION()")
-            # 使用 fetchone() 方法获取单条数据.
-            data = cursor.fetchone()
-            print("Database version : %s " % data)
+    def __init__(self):
+        self.netAddress = "localhost"
+    def MysqlCreate(self):
+        # 打开数据库连接（ip/数据库用户名/登录密码/数据库名）
+        db = pymysql.connect(self.netAddress, "root", "123456789", "twadspiderdata")
+        # 使用 cursor() 方法创建一个游标对象 cursor
+        cursor = db.cursor()
 
-            # 关闭数据库连接
-            db.close()
+        # 使用 execute()  方法执行 SQL 查询
+        cursor.execute("SELECT VERSION()")
+        # 使用 fetchone() 方法获取单条数据.
+        data = cursor.fetchone()
+        print("Database version : %s " % data)
+
+        # 关闭数据库连接
+        db.close()
